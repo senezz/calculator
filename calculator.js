1978257d8dcbd2d33f7700e458f38f25e55402d8
senezz <senezzyx@gmail.com>
2025-11-13 19:38:41 +0100

    init

---
+++ calculator.js
@@ -0,0 +1,15 @@
+// js
+// 100 + 20 * 12
+// const res = eval("10 + 2")
+// document
+// const btn = document.getElementById()
+// btn.addEventListener("click", () => {})
+// input.value
+
+// css
+// grid
+// display: grid;
+// grid-template-row: 10px 20px 30px; -- height
+// grid-template-column: 20px 30px; -- width
+
+// + - * /
