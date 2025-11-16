dd333f768e5667a13a866c6c11b13b2cf107951b
senezz <senezzyx@gmail.com>
2025-11-16 22:04:49 +0100

    added assets,theme,percent

--- calculator.js
+++ calculator.js
@@ -1,34 +1,87 @@
-// js
-// 100 + 20 * 12
-// const res = eval("10 + 2")
-// document
-// const btn = document.getElementById()
-// btn.addEventListener("click", () => {})
-// input.value
-
-// css
-// grid
-// display: grid;
-// grid-template-row: 10px 20px 30px; -- height
-// grid-template-column: 20px 30px; -- width
-
-// + - * /
-
 const numbers = document.querySelectorAll(".numbers");
 const operators = document.querySelectorAll(".operators");
 const display = document.querySelector(".display")
 const equals = document.querySelector(".equals")
 const acBtn = document.getElementById("ac-btn")
 const cBtn = document.getElementById("c-btn")
+const themeSwitchBtn = document.getElementById("theme-switch-btn")
 
-for (const number of numbers) {
-    number.addEventListener("click", () => {
-        if (Number(display.value) === 0 && number.innerText !== ",") {
-            display.value = ""
+numbers.forEach(number => number.addEventListener("click", onNumberClick))
+operators.forEach(operator => operator.addEventListener("click", onOperatorClick))
+
+equals.addEventListener("click", onEquals)
+acBtn.addEventListener("click", onClearAll)
+cBtn.addEventListener("click", onClear)
+display.addEventListener("input", onDisplay)
+themeSwitchBtn.addEventListener("click", onThemeSwitch)
+
+function onNumberClick({target: number}) {
+    if (Number(display.value) === 0 && number.innerText !== ",") {
+        display.value = ""
+    }
+
+    display.value += number.innerText
+}
+
+function onOperatorClick({target: operator}) {
+    if (Number(display.value) === 0 && operator.innerText === "–") {
+        display.value = ""
+    }
+
+    const lastIndex = display.value.length - 1
+    const isLastSymbolOp = checkIfOp(display.value[lastIndex])
+    if (isLastSymbolOp) {
+        clearAllPrevOps()
+        const canBeOp = operator.innerText === "–" && display.value[lastIndex] !== "–"
+        const slicedDisplay = display.value.slice(0, lastIndex)
+        display.value = canBeOp ? 
+            display.value + operator.innerText :
+            slicedDisplay + operator.innerText
+    } else {
+        display.value += operator.innerText
+    }
+}
+
+function onEquals() {
+    const lastIndex = display.value.length - 1
+    if (checkIfOp(display.value[lastIndex])) {
+        display.value = display.value.slice(0, lastIndex)
+    }
+
+    if (display.value[lastIndex] === '%') {
+        display.value = display.value.slice(0, lastIndex)
+    }
+
+    const subs = [["–","-"], ["x","*"], [",","."]];
+    subs.forEach(([newValue, oldValue]) => 
+        display.value = display.value.replaceAll(newValue, oldValue))
+
+    try {
+        let result = Number(eval(display.value))
+        if (Number.isNaN(result) || !Number.isFinite(result)) {
+            result = "0"
         }
+        display.value = result
+    } catch(error) {
+        console.error(error)
+    }
 
-        display.value += number.innerText
-    })
+    subs.forEach(([newValue, oldValue]) => 
+        display.value = display.value.replaceAll(oldValue, newValue))
+}
+
+function onClearAll() {
+    display.value = "0"
+}
+
+function onClear() {
+    const slicedDisplay = display.value.slice(0, display.value.length - 1)
+    display.value = display.value.length <= 1 ? "0" : slicedDisplay
+}
+
+function onDisplay(event) {
+    event.preventDefault()
+    display.value = display.value.replace(/\D/g, '');
 }
 
 function clearAllPrevOps() {
@@ -39,67 +92,23 @@ function clearAllPrevOps() {
 }
 
 function checkIfOp(symbol) {
-    const ops = ["+", "–", "x", "/"]
+    const ops = ["+", "–", "x", "/", "%"]
     return ops.includes(symbol)
 }
 
-for (const operator of operators) {
-    operator.addEventListener("click", () => {
-        if (Number(display.value) === 0 && operator.innerText === "–") {
-            display.value = ""
-        }
-
-        const lastIndex = display.value.length - 1
-        const isLastSymbolOp = checkIfOp(display.value[lastIndex])
-        if (isLastSymbolOp) {
-            clearAllPrevOps()
-            const canBeOp = operator.innerText === "–" && display.value[lastIndex] !== "–"
-            if (canBeOp) {
-                display.value += operator.innerText
-            } else {
-                display.value = display.value.slice(0, lastIndex) + operator.innerText
-            }
-        } else {
-            display.value += operator.innerText
-        }
-    })
+function onThemeSwitch() {
+    const theme = document.body.classList.toggle('light-theme') ? 'light' : 'dark'
+    localStorage.setItem('theme', theme)
+    document.getElementById("theme-switch-img")
+        .setAttribute('src', `./assets/${theme}-theme.svg`)
 }
 
-equals.addEventListener("click", () => {
-    const lastIndex = display.value.length - 1
-    if (checkIfOp(display.value[lastIndex])) {
-        display.value = display.value.slice(0, lastIndex)
-    }
-
-    display.value = display.value.replaceAll("–","-")
-    display.value = display.value.replaceAll("x","*")
-    display.value = display.value.replaceAll(",",".")
-    try {
-        display.value = eval(display.value)
-    } catch(error) {
-        console.error(error)
-    }
-
-    display.value = display.value.replaceAll("-","–")
-    display.value = display.value.replaceAll("*","x")
-    display.value = display.value.replaceAll(".",",")
-})
-
-acBtn.addEventListener("click", () => {
-    display.value = "0"
-})
-
-cBtn.addEventListener("click", () => {
-    if (display.value.length <= 1) {
-        display.value = "0"
-    } else {
-        display.value = display.value.slice(0, display.value.length - 1)
-    }
-})
-
-display.addEventListener("input", (event) => {
-    event.preventDefault()
-    display.value = display.value.replace(/\D/g, '');
-})
-
-
+const savedTheme = localStorage.getItem('theme')
+if (savedTheme === 'light') {
+    document.body.classList.add('light-theme')
+    document.getElementById("theme-switch-img")
+        .setAttribute('src', './assets/light-theme.svg')
+} else {
+    document.getElementById("theme-switch-img")
+        .setAttribute('src', './assets/dark-theme.svg')
+}
